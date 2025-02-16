8adc51153df7132804b6acc66d1ef9abb946bd51
youness anflous <anayouness51@gmail.com>
2025-02-16 18:19:10 +0100

    update mac

--- MAC_Address_Changer/Mac_changer.py
+++ MAC_Address_Changer/Mac_changer.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
 
-# learn about the subprocess, optparse
 import subprocess
 import optparse
 
-# basic
-parser = optparse.OptionParser() # what is it and why
+def		parse_args():
+    parse = optparse.OptionParser()
+    parse.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address.")
+    parse.add_option("-m", "--mac", dest="new_mac", help="MAC Address to set it to the interface")
+    return parse.parse_args()
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to chagne its MAC Address")
-parser.add_option("-m", "--mac", dest="new_mac", help="The New MAC Address")
+def		change_mac(interface, new_mac):
+    subprocess.run(["ifconfig", interface, "down"])
+    subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.run(["ifconfig", interface, "up"])
 
-# how that (...) works.
-(options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
-
-# basic basic
-# interface = input("Interface -> ")
-# new_mac = input("New MAC -> ")
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig" , interface, "up"])
- 
-print("Your interface " + interface + " is updated with the new MAC Address " + new_mac)
+# problem with the no args
+# problem with jsut -m of -i and both -m and -i without a value of it !!
+(options, arguments) = parse_args()
+change_mac(options.interface, options.new_mac)
