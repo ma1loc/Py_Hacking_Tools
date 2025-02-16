b3f5145b66704aab12c67dc14ede66716a7d866f
Younes Anflous <anayouness51@gmail.com>
2025-02-16 20:35:20 +0100

    updated

--- MAC_Address_Changer/Mac_changer.py
+++ MAC_Address_Changer/Mac_changer.py
@@ -3,19 +3,21 @@
 import subprocess
 import optparse
 
-def		parse_args():
-    parse = optparse.OptionParser()
-    parse.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address.")
-    parse.add_option("-m", "--mac", dest="new_mac", help="MAC Address to set it to the interface")
-    return parse.parse_args()
+def	parse_args():
+	parse = optparse.OptionParser()
+	parse.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address.")
+	parse.add_option("-m", "--mac", dest="new_mac", help="MAC Address to set it to the interface")
+	(options, arguments) = parse.parse_args()
+	if not options.interface:
+    		parse.error("[-] Error There's no provided Interface, use --help for clear usge.")
+	elif not options.new_mac:
+    		parse.error("[-] Error There's no provided MAC Addr, use --help for clear usge.")
+	return options
 
-def		change_mac(interface, new_mac):
+def change_mac(interface, new_mac):
     subprocess.run(["ifconfig", interface, "down"])
     subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.run(["ifconfig", interface, "up"])
 
-
-# problem with the no args
-# problem with jsut -m of -i and both -m and -i without a value of it !!
-(options, arguments) = parse_args()
+options = parse_args()
 change_mac(options.interface, options.new_mac)
